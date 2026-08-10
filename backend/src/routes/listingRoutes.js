5ad5b028931e0870284a50d5c29ee0a9e27d45f2
Camanze-alt72 <camanze@my.centennialcollege.ca>
2026-08-10 10:30:40 -0400

    Add available listings endpoint

--- backend/src/routes/listingRoutes.js
+++ backend/src/routes/listingRoutes.js
@@ -8,12 +8,14 @@ const {
     getListingById,
     getListingsByDonor,
     updateListing,
+    getAvailableListings,
     deleteListing
 } = require("../controllers/listingController");
 
 router.post("/", createListing);
 router.get("/", getAllListings);
 router.get("/donor/:id", getListingsByDonor);
+router.get("/available", getAvailableListings);
 router.get("/:id", getListingById);
 router.put("/:id", updateListing);
 router.delete("/:id", deleteListing);
